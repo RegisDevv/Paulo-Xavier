25c3476616def272a5408ad9a79daef8b7fc0422
Regis Dev <93346482+RegisDevv@users.noreply.github.com>
2022-07-27 02:04:16 -0300

    Paulo Xavier

---
+++ script.js
@@ -0,0 +1,12 @@
+(function () {
+    var options = {
+        whatsapp: "5511981950165",
+        call_to_action: "Ligue já",
+        button_color: "#34af23",
+        position: "right",
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
